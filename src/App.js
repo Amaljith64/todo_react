9e89366e32b1d359cfd9368d0e7fb4c638f0f114
Amaljith <amaljith64@gmail.com>
2022-10-30 15:31:35 +0530

    editing option added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import {useState} from 'react'
+import Swal from "sweetalert2";
+
 
 function App() {
   const [toDos, setToDos] = useState([])
@@ -58,6 +60,27 @@ function App() {
   const resetInputField =()=>{
     setTodo('')
   }
+
+
+  const editToggle = async (id) => {
+   const { value: editedTask } = await Swal.fire({
+     title: "Rename Task",
+     input: "text",
+     inputValue:toDos.find((todo)=>todo.id === id).text,
+     inputPlaceholder: "Enter Here",
+   });
+
+   if (editedTask) {
+     let newTodos =toDos.filter((todo) => {
+       if (todo.id === id) {
+         todo.text = editedTask;
+       }
+       return todo;
+     })
+     setToDos(newTodos);
+
+   }
+ };
   return (
     <>
     <div className="headings">
@@ -108,7 +131,11 @@ function App() {
                                     return obj2;
                                  }));
                               }} value={obj.statusRemove} className="fas fa-trash-alt" title="Remove"></i>
-                           </div>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={
+                              () => editToggle(obj.id)}>
+                           <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                           </svg>
+                           </div>                          
                         </div>
                      );
                   }
@@ -151,6 +178,10 @@ function App() {
                                     return obj2;
                                  }));
                               }} value={obj.statusDrop} className="fas fa-times" title="Drop"></i>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={
+                              () => editToggle(obj.id)}>
+                           <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                           </svg>
                            </div>
                         </div>
                      );
@@ -186,6 +217,10 @@ function App() {
                                     return obj2;
                                  }));
                               }} value={obj.statusDrop} className="fas fa-times" title="Drop"></i>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={
+                              () => editToggle(obj.id)}>
+                           <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                           </svg>
                            </div>
                         </div>
                      );
